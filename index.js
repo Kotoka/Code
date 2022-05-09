4be9da7de25248fd6d28c1de256e4ad9e4c2952f
Kotoka <104259927+Kotoka@users.noreply.github.com>
2022-05-09 18:58:04 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let foodEl = document.getElementById("food-el")
+
+
+function variety() {
+    foodEl.textContent = "FOOD IS GOOD FOR THE HEALTH, AND WE NEED TO EAT EVERYDAY IN WANNA LIFE.SO BOSS ANOD PLEASE EAT FOR ME."
+
+
+}
