134a7e6319e1cf7e8e92341a10766e0ce7ebb4dc
Kotoka <104259927+Kotoka@users.noreply.github.com>
2022-06-10 03:17:26 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-let foodEl = document.getElementById("food-el")
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
 
-
-function variety() {
-    foodEl.textContent = "FOOD IS GOOD FOR THE HEALTH, AND WE NEED TO EAT EVERYDAY IN WANNA LIFE.SO BOSS ANOD PLEASE EAT FOR ME."
-
-
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
 }
